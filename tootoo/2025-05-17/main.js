b052411eb593d8a4b444659226adc1ed6233201e
Theo Armour <t.armour@gmail.com>
2025-05-17 11:25:27 -0700

    Add initial implementation of TooToo 2025 with GitHub API integration
    - Created tootooedit.html for editing functionality with navigation menu and iframe for content display.
    - Developed github-tree-view.html and corresponding JS for displaying repository structure.
    - Implemented main.js to manage core functionalities and configurations.
    - Added style-core.css for consistent styling across the application.
    - Established tootoo.html as the main entry point, integrating all components and scripts.
    - Included responsive design features and accessibility improvements.

---
+++ tootoo/2025-05-17/main.js
@@ -0,0 +1,22 @@
+
+const COR = {
+
+    // Used by GFO ~ github file open
+    defaultFile: "README.md",
+    pathContent: "../../",
+
+    // Used by GTV ~ github tree view
+    user: "pushme-pullyou",
+    repo: "tootoo-2025",
+    branch: "main",
+
+    urlSource: "https://github.com/pushme-pullyou/tootoo-2025/tree/main/tootoo",
+    urlPathContent: "https://pushme-pullyou.github.io/tootoo-2025/",
+
+    menuTitle: "TooToo 2025",
+    version: "Version: 2025-05-17 10:41",
+
+    filterFolders: [], //[ "code"],
+
+    ignoreFiles: [], //[ "404.html", "favicon.ico", "index.html", "LICENSE", "readme.html" ],
+}
