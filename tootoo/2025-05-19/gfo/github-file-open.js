fb3932ef8ae3cbd585dc96a90760341cfa2979b0
Theo Armour <t.armour@gmail.com>
2025-05-19 19:48:31 -0700

    Enhance HTML files with metadata, fix file paths, and improve layout styles

--- tootoo/2025-05-19/gfo/github-file-open.js
+++ tootoo/2025-05-19/gfo/github-file-open.js
@@ -1,61 +1,36 @@
-
 initGFO();
 
 function initGFO() {
 
-  showdown.setFlavor("github");
-
-  if (!location.hash) { location.hash = COR.defaultFile; }
-
-  if (location.hash.endsWith(".md")) {
-
-    onHashChange();
-
-  } else {
-
-    const url = COR.pathContent + location.hash.slice(1);
-
-    divMainContent.innerHTML = `<iframe class=iframe-resize src="${ url }" height=100% width=100% ><iframe>`;
-
-  }
+  //location.hash = COR.pathContent + COR.defaultFile;
 
   window.addEventListener("hashchange", onHashChange, false);
 
-}
+  onHashChange();
 
+}
 
 function onHashChange() {
 
-  // if no file extension then probably an internal link
-  if (!location.hash.includes(".")) { return; }
+  console.log( "location.hash", location.hash );
 
-  const url = COR.pathContent + location.hash.slice(1);
+  if ( location.hash.includes( "https://api.github.com")) {
 
-  let title =  url.split("/").pop()
-    .split("-")
-    .filter(x => x.length > 0)
-    .map((x) => (x.charAt(0).toUpperCase() + x.slice(1)))
-    .join(" ");
-  document.title = title;
+    console.log( "notesy",  );
 
-  if (location.hash.endsWith(".md")) {
-
-    const options = { openLinksInNewWindow: false, excludeTrailingPunctuationFromURLs: true, ghMention: true, simplifiedAutoLink: true, simpleLineBreaks: true, emoji: true };
-    const xhr = new XMLHttpRequest();
-    xhr.open("get", url, true);
-    xhr.onload = () => {
-      let txt = xhr.responseText;
-      //txt = txt.replace(/\<!--@@@/, "").replace(/\@@@-->/, "");
-      divMainContent.innerHTML = new showdown.Converter(options).makeHtml(txt);
-      window.scrollTo(0, 0);
-    };
-    xhr.send(null);
-
+    divMainContent.innerHTML = `<iframe id=ifr class="iframe-resize" src="../edit/notesy.html" onload=update() height=100% width=100% ><iframe>`;
+  
   } else {
 
-    divMainContent.innerHTML = `<iframe id=ifr class=iframe-resize src="${ url }" height=100% width=100% ><iframe>`;
+    divMainContent.innerHTML = `<iframe id=ifr class="iframe-resize" src="readme.html" onload=update() height=100% width=100% ><iframe>`;
 
   }
+
+}
+
+function update() {
+
+  ifr.contentWindow.location.hash = parent.location.hash;
 
 }
 
