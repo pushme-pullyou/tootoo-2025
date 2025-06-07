15a96bab6bec5f146ee3fd123b5ff9d0fa219ab6
Theo Armour <t.armour@gmail.com>
2025-06-07 16:45:32 -0700

    Refactor content handling in GitHub file operations and update iframe structure in TooToo editor

--- tootoo/2025-06-07/gfo/github-file-open.js
+++ tootoo/2025-06-07/gfo/github-file-open.js
@@ -1,6 +1,12 @@
+let divContent;
+
 function onHashChange() {
 
   COR.hash = hash = location.hash.slice(1);
+  
+  divContent = COR.divContent;
+
+  console.log( "div", divContent );
 
   // Validate hash to prevent potential security issues
   if (!hash || hash.trim() === '') {
@@ -31,8 +37,7 @@ function onHashChange() {
       console.log("notesy", COR.hash);
 
       //COR.hash = hash.replace("@@", "");
-
-      divMainContent.innerHTML =
+      divContent.innerHTML =
 
         `<iframe id=ifr class="iframe-resize" onload=onLoad() src="${COR.pathApps}/ggpf/github-get-put-file.html"><iframe>`;
 
@@ -47,8 +52,8 @@ function onHashChange() {
   } else if (/\.(jpg|jpeg|png|gif|svg|ico|bmp|tiff|webp)$/i.test(hash)) {
 
     console.log("img", hash);
-
-    divMainContent.innerHTML = `<img src="${COR.pathContent}${hash}" ></img>`;
+    
+    divContent.innerHTML = `<img src="${COR.pathContent}${hash}" ></img>`;
 
   } else if (hash === "LICENSE") {
 
@@ -59,8 +64,7 @@ function onHashChange() {
   } else {
 
     console.log("else", COR.urlPathContent + hash);
-
-    divMainContent.innerHTML = `<iframe id=ifr class="iframe-resize" src="${COR.urlPathContent}${hash}" ></iframe>`
+    divContent.innerHTML = `<iframe id=ifr class="iframe-resize" src="${COR.urlPathContent}${hash}" ></iframe>`
   }
 
   // see github-tree-view.js
@@ -100,7 +104,7 @@ function onLoad() {
 
 function getHTMLfromURL(hash = COR.hash) {
 
-  //console.log("hash", COR.pathContent + COR.hash);
+  console.log("hash", COR.pathContent + COR.hash);
 
   showdown.setFlavor("github");
   const options = { openLinksInNewWindow: false, excludeTrailingPunctuationFromURLs: true, ghMention: true, simplifiedAutoLink: true, simpleLineBreaks: true, emoji: true };
@@ -109,20 +113,11 @@ function getHTMLfromURL(hash = COR.hash) {
   xhr.open("get", COR.pathContent + hash, true);
   xhr.onload = () => {
     let txt = xhr.responseText;
-    txt = txt.replace(/<!--@@@/g, "").replace(/@@@-->/g, "");
-    divMainContent.innerHTML = new showdown.Converter(options).makeHtml(txt);
+    //txt = txt.replace(/<!--@@@/g, "").replace(/@@@-->/g, "")
+    divContent.innerHTML = new showdown.Converter(options).makeHtml(txt);
     window.scrollTo(0, 0);
   };
-  xhr.onerror = () => {
-    divMainContent.innerHTML = `
-      <div style="padding: 20px; color: #d73a49; background: #ffeaea; border: 1px solid #d73a49; border-radius: 4px;">
-        <h3>Network Error</h3>
-        <p>Failed to load file: ${hash}</p>
-        <p>Please check your internet connection and try again.</p>
-        <button onclick="onHashChange()" style="margin-top: 10px; padding: 5px 10px;">Retry</button>
-      </div>
-    `;
-  };
+  xhr.onerror = (e) => { console.log( "e", e); };
   xhr.send(null);
 
   let title = hash.split("/").pop()
