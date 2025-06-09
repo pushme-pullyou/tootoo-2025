c1f7c0a46bf8193de370e6c39d46dd4051273379
Theo Armour <t.armour@gmail.com>
2025-06-09 13:38:55 -0700

    Update URL source in main.js and adjust link behavior in tootoo.html; enhance README with recent changes

--- tootoo/2025-06-09/main.js
+++ tootoo/2025-06-09/main.js
@@ -20,7 +20,7 @@ const COR = {
   ignoreFiles: CONFIG.ignoreFiles, //[], //[ "404.html", "favicon.ico", "index.html", "LICENSE", "readme.html" ],
 
   urlBaseAPI: `https://api.github.com/repos/${CONFIG.user}/${CONFIG.repo}/contents/`,
-  urlSource: `https://github.com/${CONFIG.user}/${CONFIG.repo}/tree/main/`,
+  urlSource: `https://github.com/${CONFIG.user}/${CONFIG.repo}/`,
   urlPathApps: `https://${CONFIG.user}.github.io/${folder}/tootoo/`,
   urlPathContent: `https://${CONFIG.user}.github.io/${folder}/`,
   urlPushPath: `https://${CONFIG.user}.github.io/${folder}/`,
